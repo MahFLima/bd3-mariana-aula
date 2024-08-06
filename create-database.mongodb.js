51ec1b8124126ea01796dd6500cd511ee1d6f434
Mariana Lima <mariana.lima118@outlook.com>
2024-08-06 20:35:08 -0300

    first commit

---
+++ create-database.mongodb.js
@@ -0,0 +1,12 @@
+/* Nome do banco de dados */
+const database = 'BD3-MARIANA-AULA'
+
+/* Nome da coleção de dados */
+const collection = 'LIVRARIA'
+
+/* CRIAR OU ACESSAR O BANCO DE DADOS */
+use(database)
+
+/* CRIAR UMA COLEÇÃO DE DADOS */
+db.createCollection(collection)
+
