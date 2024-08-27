6cc7f9874203e7cd23633552de218b1af331bd61
Mariana Lima <mariana.lima118@outlook.com>
2024-08-27 20:36:53 -0300

    Tipos de seleção

---
+++ find_where_fields_like.mongodb.js
@@ -0,0 +1,15 @@
+/* Nome do banco de dados */
+const database = "BD3-MARIANA-AULA";
+
+/* Nome da coleção de dados */
+const collection = "LIVRARIA";
+
+/* CRIAR OU ACESSAR O BANCO DE DADOS */
+use(database);
+
+/* Seleciona todos os registros da seleção */
+db[collection].find(
+  {"descricao": /robôs/i},
+  {"_id": 0, "codigo": 0}
+)
+
